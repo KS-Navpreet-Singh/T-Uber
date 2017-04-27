233943d43f937332d5d9afb95d1ef39a22c03548
Navpreet Singh <navpreet.singh233@gmail.com>
2017-04-26 21:27:46 -0500

    login tutor, loing student, and sign page linked to the main page.

--- app/src/main/java/com/example/nav/t_uber/LoginStudent.java
+++ app/src/main/java/com/example/nav/t_uber/LoginStudent.java
@@ -3,6 +3,7 @@ package com.example.nav.t_uber;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -66,6 +67,7 @@ public class LoginStudent extends AppCompatActivity implements LoaderCallbacks<C
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_student);
+
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
         populateAutoComplete();
@@ -81,12 +83,19 @@ public class LoginStudent extends AppCompatActivity implements LoaderCallbacks<C
                 return false;
             }
         });
-
+//----------------
         Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 attemptLogin();
+
+                Intent Sign_in = new Intent(LoginStudent.this, activity_student.class);
+
+                startActivity(Sign_in);
+
+
             }
         });
 
--- app/src/main/java/com/example/nav/t_uber/SignUp.java
+++ app/src/main/java/com/example/nav/t_uber/SignUp.java
@@ -1,13 +1,65 @@
 package com.example.nav.t_uber;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SignUp extends AppCompatActivity {
+
+    public Button Tutor;
+    public Button Student;
+
+
+    //After Tutor Sign Up, goes back to main page
+    public void tutor() {
+        Tutor = (Button) findViewById(R.id.tutor_button);
+        Tutor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent SignUp_Tutor = new Intent(SignUp.this, MainActivity.class);
+
+                startActivity(SignUp_Tutor);
+
+            }
+        });
+
+    }
+
+
+    //After Student Sign Up, goes back to main page
+    public void student() {
+        Student = (Button) findViewById(R.id.student_button);
+        Student.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent SignUp_Student = new Intent(SignUp.this, MainActivity.class);
+
+                startActivity(SignUp_Student);
+
+            }
+        });
+
+    }
+
+
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        tutor();
+        student();
+
     }
 }
