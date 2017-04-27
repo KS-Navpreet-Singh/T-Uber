a112ca56a1d6ae8fc02a74b45f6218c4e46d6b49
Navpreet Singh <navpreet.singh233@gmail.com>
2017-04-26 19:12:37 -0500

    MAIN

--- app/src/main/java/com/example/nav/t_uber/MainActivity.java
+++ app/src/main/java/com/example/nav/t_uber/MainActivity.java
@@ -1,11 +1,33 @@
 package com.example.nav.t_uber;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public Button Tutor;
+
+        public void init() {
+            Tutor = (Button) findViewById(R.id.tutor);
+            Tutor.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    Intent login_Tutor = new Intent(MainActivity.this, LoginTutor.class);
+
+                    startActivity(login_Tutor);
+
+                }
+            });
+
+        }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +40,6 @@ public class MainActivity extends AppCompatActivity {
         //--
 
         setContentView(R.layout.activity_main);
+        init();
     }
 }
