a8d7753801932992a2844deabbd754add4ef3f82
Navpreet Singh <navpreet.singh233@gmail.com>
2017-05-03 15:25:14 -0500

    database is working

--- app/src/main/java/com/example/nav/t_uber/SignUp.java
+++ app/src/main/java/com/example/nav/t_uber/SignUp.java
@@ -1,7 +1,6 @@
 package com.example.nav.t_uber;
 
 import android.app.ProgressDialog;
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,14 +16,14 @@ import java.util.HashMap;
 public class SignUp extends AppCompatActivity {
 
     Button sign_Up_Student;
-    EditText First_Name, Last_Name, Email, Password;
-    String str_First_Name, str_Last_Name, str_Email, str_Password;
+    Button sign_Up_Tutor;
+    EditText First_Name, Last_Name, Email, Password1, Password2;
+    String str_First_Name, str_Last_Name, str_Email, str_Password1, str_Password2;
     String finalResult;
-    String HttpURL = "https://people.eecs.ku.edu/~nsingh94/Register.php";
+    String HttpURL = "https://people.eecs.ku.edu/~apodgorn/tuber/create_profile.php";
     ProgressDialog progressDialog;
     HashMap<String,String> hashMap = new HashMap<>();
     HttpParse httpParse = new HttpParse();
-
 
 
     @Override
@@ -36,39 +35,75 @@ public class SignUp extends AppCompatActivity {
         First_Name = (EditText)findViewById(R.id.FirstName);
         Last_Name = (EditText)findViewById(R.id.LastName);
         Email = (EditText)findViewById(R.id.StudenEmail);
-        Password = (EditText)findViewById(R.id.UserPassword);
+        Password1 = (EditText)findViewById(R.id.password1);
+        Password2 = (EditText)findViewById(R.id.password2);
 
         sign_Up_Student = (Button)findViewById(R.id.student_Button);
+        sign_Up_Tutor = (Button)findViewById(R.id.tutor_button);
 
 
-        //Adding Click Listener on button.
+        //Adding Click Listener on button for Student.
         sign_Up_Student.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                String status = "Student";
+
                 str_First_Name = First_Name.getText().toString();
                 str_Last_Name = Last_Name.getText().toString();
                 str_Email = Email.getText().toString();
-                str_Password = Password.getText().toString();
+                str_Password1 = Password1.getText().toString();
+                str_Password2 = Password2.getText().toString();
 
 
-                if(TextUtils.isEmpty(str_First_Name) || TextUtils.isEmpty(str_Last_Name) || TextUtils.isEmpty(str_Email) || TextUtils.isEmpty(str_Password)){
+                if(TextUtils.isEmpty(str_First_Name) || TextUtils.isEmpty(str_Last_Name) || TextUtils.isEmpty(str_Email) || TextUtils.isEmpty(str_Password1)
+                        || TextUtils.isEmpty(str_Password2) ){
 
                     Toast.makeText(SignUp.this, "Please fill all form fields.", Toast.LENGTH_LONG).show();
                 }else{
 
-                    Register(str_First_Name, str_Last_Name, str_Email, str_Password);
+                    Register(str_First_Name, str_Last_Name, str_Email, str_Password1, str_Password2, status);
 
                 }
 
             }
         });
 
-    }
+
+
+    //Adding Click Listener on button for Tutor.
+        sign_Up_Tutor.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+            String status = "Tutor";
+
+            str_First_Name = First_Name.getText().toString();
+            str_Last_Name = Last_Name.getText().toString();
+            str_Email = Email.getText().toString();
+            str_Password1 = Password1.getText().toString();
+            str_Password2 = Password2.getText().toString();
+
+
+            if(TextUtils.isEmpty(str_First_Name) || TextUtils.isEmpty(str_Last_Name) || TextUtils.isEmpty(str_Email) || TextUtils.isEmpty(str_Password1)
+                    || TextUtils.isEmpty(str_Password2) ){
+
+                Toast.makeText(SignUp.this, "Please fill all form fields.", Toast.LENGTH_LONG).show();
+            }else{
+
+                Register(str_First_Name, str_Last_Name, str_Email, str_Password1, str_Password2, status);
+
+            }
+
+        }
+    });
+
+}
 
 
 
-    public void Register(final String F_Name, final String L_Name, final String email, final String password){
+    public void Register(final String F_Name, final String L_Name, final String email, final String password1, final String password2, final
+                         String status){
 
         class RegisterClass extends AsyncTask<String,Void,String> {
 
@@ -96,11 +131,15 @@ public class SignUp extends AppCompatActivity {
 
                 hashMap.put("f_name",params[0]);
 
-                hashMap.put("L_name",params[1]);
+                hashMap.put("l_name",params[1]);
 
-                hashMap.put("email",params[2]);
+                hashMap.put("email_address",params[2]);
 
-                hashMap.put("password",params[3]);
+                hashMap.put("password1",params[3]);
+
+                hashMap.put("password2",params[4]);
+
+                hashMap.put("status",params[5]);
 
                 finalResult = httpParse.postRequest(hashMap, HttpURL);
 
@@ -111,7 +150,7 @@ public class SignUp extends AppCompatActivity {
         }
 
         RegisterClass Register = new RegisterClass();
-        Register.execute(F_Name,L_Name,email,password);
+        Register.execute(F_Name,L_Name,email,password1, password2,status);
 
     }
 
