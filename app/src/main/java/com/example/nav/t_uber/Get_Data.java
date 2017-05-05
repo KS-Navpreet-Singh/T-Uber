d1db7353f58378d718f4bc97f20c7050fd4393f2
Navpreet Singh <navpreet.singh233@gmail.com>
2017-05-04 22:50:00 -0500

    sign up and login is working

---
+++ app/src/main/java/com/example/nav/t_uber/Get_Data.java
@@ -0,0 +1,20 @@
+package com.example.nav.t_uber;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+/**
+ * Created by Nav on 5/4/2017.
+ */
+
+
+
+public class Get_Data extends Activity {
+
+}
--- app/src/main/java/com/example/nav/t_uber/myProfileFragment.java
+++ app/src/main/java/com/example/nav/t_uber/myProfileFragment.java
@@ -1,13 +1,13 @@
 package com.example.nav.t_uber;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.TextView;
+
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,27 +21,20 @@ public class myProfileFragment extends Fragment {
     }
 
 
-    TextView first_name;
-
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_my_profile, container, false);
 
-
-
         ((TextView)v.findViewById(R.id.textView6)).setText("UserEmail@ku.edu");
 
         return v;
+
+
     }
 
-
-
-
-//----------- change ActionBar title
+    //----------- change ActionBar title
     public void onResume(){
         super.onResume();
 
@@ -50,9 +43,5 @@ public class myProfileFragment extends Fragment {
                 .setActionBarTitle("My Profile");
 
     }
-
-
-
-
 
 }
